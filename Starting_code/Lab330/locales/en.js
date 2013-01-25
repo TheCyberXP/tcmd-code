c47a82967f7e95e41f4181f7e4e0883efeeda481
Tim Poulsen <tim@skypanther.com>
2013-01-24 16:54:20 -0800

    Fix: Wrong lab code for 330 - fixing starting code

---
+++ Starting_code/Lab330/locales/en.js
@@ -0,0 +1,12 @@
+{
+	"creates a new mobile application or module": "creates a new mobile application or module",
+	"a platform to clean": "a platform to clean",
+	"the directory containing the project, otherwise the current working directory": "the directory containing the project, otherwise the current working directory",
+	"Titanium SDK version to use": "Titanium SDK version to use",
+	"minimum logging level": "minimum logging level",
+	"level": "level",
+	"Touching tiapp.xml": "Touching tiapp.xml",
+	"Deleting all platform build directories": "Deleting all platform build directories",
+	"Deleting %s": "Deleting %s",
+	"Project cleaned successfully in %s": "Project cleaned successfully in %s"
+}
